5f42c3e8f76e8af70a3282cb6e5f613cf0181e9a
killvung <killvung@gmail.com>
2020-11-20 16:17:43 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+const App = () => (
+  <div>
+    <h1>WriteBoard</h1>
+    <h2>In Construction</h2>
+  </div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack").container.ModuleFederationPlugin;
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 8081,
+  },
+  output: {
+    publicPath: "auto",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /bootstrap\.tsx$/,
+        loader: "bundle-loader",
+        options: {
+          lazy: true,
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react", "@babel/preset-typescript"],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "wdr_ui_draw",
+      filename: "remoteEntry.js",
+      exposes: {
+        './App': "./src/App"
+      },
+      shared: ["react", "react-dom"],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
